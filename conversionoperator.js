39294d0516691792caeb5069749f8f6e0cb634fc
Rajdeep choudhury <153299193+Rajdeepry@users.noreply.github.com>
2025-06-19 21:52:49 +0530

    Add files via upload

---
+++ conversionoperator.js
@@ -0,0 +1,15 @@
+let score = "33avb"
+
+//console.log(typeof(score));//
+
+//let valueinnumber = Number(score);
+//console.log(typeof valueinnumber);
+
+
+let isLoggedin = 0
+
+//console.log(typeof isLoggedin)
+
+let valueinboolean = Boolean(isLoggedin);
+console.log(typeof valueinboolean);
+console.log(valueinboolean);
