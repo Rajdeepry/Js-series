9195cf1de11591016969e1d1df2c9edad0c9b029
Rajdeep choudhury <153299193+Rajdeepry@users.noreply.github.com>
2025-07-06 23:33:30 +0530

    Add files via upload

---
+++ Dates.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2025, 0, 23)
+console.log(myCreatedDate.toString());
