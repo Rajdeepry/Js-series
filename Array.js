5f6cf1207e20c0334540aedfcf8b78cc250e7fde
Rajdeep choudhury <153299193+Rajdeepry@users.noreply.github.com>
2025-07-07 22:59:44 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,36 @@
+//Array
+
+const myArr = [0,1,2,3,4]
+
+const myHeros = ["ironman", "Captaion America" ,"Hulk"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+//array methods
+
+
+myArr.push(6)
+
+console.log(myArr);
+
+myArr.pop()
+
+//myArr.shift()
+//myArr.unshift()
+
+//const newArr = myArr.join()
+
+//console.log(newArr);
+
+
+
+//console.log(myArr);
+
+//slice , splice 
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr)
