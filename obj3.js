25e9a683438d95b948355af096f76b480e1a5042
Rajdeep choudhury <153299193+Rajdeepry@users.noreply.github.com>
2025-07-09 01:54:37 +0530

    Add files via upload

---
+++ obj3.js
@@ -0,0 +1,26 @@
+const course = {
+
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor:"Raj"
+}
+
+//course.courseInstructor
+//const {courseInstructor} = course
+//console.log(courseInstructor);
+
+{
+    "name":"raj",
+    "coursename": "js in Hindi",
+    "price": "free"
+}
+
+[
+
+    {},
+
+    {},
+
+
+    {}
+]
