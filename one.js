93d3062e29827135b84fd01a1c402b88978933b6
Rajdeep choudhury <153299193+Rajdeepry@users.noreply.github.com>
2025-07-16 02:57:16 +0530

    Update one.js

--- one.js
+++ one.js
@@ -1,10 +1,53 @@
 //if
-const isUserloggedIn=true
-const temperature = 60
-if (temperature <50){
-    console.log("less than 50");
+//const isUserloggedIn=true
+//const temperature = 60
+//if (temperature <50){
+   // console.log("less than 50");
 
-} else{
-console.log("temperature is greater than 50")}
+//} else{
+//console.log("temperature is greater than 50")}
 
-2<=2
+//const score = 201
+
+//if (score > 100) {
+   // const power = "fly"
+   // console.log(`user power: ${power}`);
+//}
+
+ //console.log(`user power: ${power}`);
+
+ //onst balance = 1000
+
+ //if (balance > 500) console.log("test");console.log("test2");
+
+ //if (balance < 500) {
+   // console.log("less than 500");
+    
+
+// } else if (balance < 750)
+ //{
+  //  console.log("less than 750");
+ //}
+ // else if (balance < 900){
+ //console.log("less than 700");
+ //}
+
+ //else {
+   // console.log("less than 1200");
+    
+ //}
+
+ const UserloggedIn = true
+ const debitCard = true
+ const loggedinFromGoogle = false
+ const loggedinFromEmail = true
+ 
+
+ if (UserloggedIn && debitCard && 2==2) {
+   console.log("Allow to buy course");
+ }
+    if (loggedinFromGoogle || loggedinFromEmail) {
+      console.log("User logged in");
+
+    }
+ 
