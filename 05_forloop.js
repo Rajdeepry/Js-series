3dc93d7c9caeeda7ac63ac349f9f8da3eccbb64a
Rajdeep choudhury <153299193+Rajdeepry@users.noreply.github.com>
2025-07-20 19:44:40 +0530

    Add files via upload

---
+++ 05_forloop.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js : "Javascript",
+    cp : "c++" ,
+    rb : "rubby",
+    swift : "swiftby apple"
+}
+for (const key in myObject) {
+   //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+ 
+for (const key in programming) {
+
+console.log(programming[key]);
+              
+     
+}
+
+
